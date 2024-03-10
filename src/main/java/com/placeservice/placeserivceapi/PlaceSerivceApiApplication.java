4bcf89c031f25ec3229d0208ba8239d5f6d7c701
João Edson Dantas <114243172+joaoedson-dantas@users.noreply.github.com>
2024-03-10 13:19:21 -0300

    first commit

---
+++ src/main/java/com/placeservice/placeserivceapi/PlaceSerivceApiApplication.java
@@ -0,0 +1,13 @@
+package com.placeservice.placeserivceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceSerivceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceSerivceApiApplication.class, args);
+	}
+
+}
